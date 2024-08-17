5675ca0933b9b73eddc90ed4ba2c9858509fa91f
TanNgo11 <tan.ngo.cit20@eiu.edu.vn>
2024-08-17 20:33:49 +0700

    init-2

--- src/main/java/com/thanhtan/identity/IdentityApplication.java
+++ src/main/java/com/thanhtan/identity/IdentityApplication.java
@@ -7,8 +7,8 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 @EnableFeignClients
 public class IdentityApplication {
-
-    public static void main(String[] args) {
+//test
+                public static void main(String[] args) {
         SpringApplication.run(IdentityApplication.class, args);
     }
 
