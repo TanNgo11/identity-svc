1d0c96b428449e904c2f3d85fa2a710ecf61a719
TanNgo11 <tan.ngo.cit20@eiu.edu.vn>
2024-08-23 17:10:12 +0700

    feat/update-format-code

--- src/main/java/com/thanhtan/identity/controller/UserController.java
+++ src/main/java/com/thanhtan/identity/controller/UserController.java
@@ -1,23 +1,25 @@
 package com.thanhtan.identity.controller;
 
+import static com.thanhtan.identity.constant.PathConstant.API_V1_USERS;
+
+import java.util.List;
+
+import jakarta.validation.Valid;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
 import com.thanhtan.identity.dto.request.UserCreationRequest;
 import com.thanhtan.identity.dto.response.ApiResponse;
 import com.thanhtan.identity.dto.response.UserResponse;
 import com.thanhtan.identity.service.IUserService;
+
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
-import jakarta.validation.Valid;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-
-import static com.thanhtan.identity.constant.PathConstant.API_V1_USERS;
-
 
 @RestController
 @RequestMapping(API_V1_USERS)
@@ -31,7 +33,6 @@ public class UserController {
     @PostMapping("/registration")
     ApiResponse<UserResponse> createUser(@RequestBody @Valid UserCreationRequest request) {
         return ApiResponse.success(userService.createUser(request));
-
     }
 
     @Operation(security = @SecurityRequirement(name = "bearerAuth"))
@@ -41,16 +42,15 @@ public class UserController {
         return ApiResponse.success(userService.getMyInfo());
     }
 
-
-//    @PutMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-//    @PreAuthorize("hasRole('USER')||hasRole('ADMIN')")
-//    public ApiResponse<UserResponse> updateUserProfile(
-//            @RequestPart("user") String userStr,
-//            @RequestPart(value = "file", required = false) MultipartFile file) throws IOException {
-//        UpdateUserRequest updateUserRequest = ValidationUtil.validateUserStr(userStr);
-//
-//        return ApiResponse.success(userService.updateUserProfile(updateUserRequest, file));
-//    }
+    //    @PutMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    //    @PreAuthorize("hasRole('USER')||hasRole('ADMIN')")
+    //    public ApiResponse<UserResponse> updateUserProfile(
+    //            @RequestPart("user") String userStr,
+    //            @RequestPart(value = "file", required = false) MultipartFile file) throws IOException {
+    //        UpdateUserRequest updateUserRequest = ValidationUtil.validateUserStr(userStr);
+    //
+    //        return ApiResponse.success(userService.updateUserProfile(updateUserRequest, file));
+    //    }
 
     @GetMapping
     @PreAuthorize("hasRole('ADMIN')||hasRole('STAFF')")
@@ -82,37 +82,37 @@ public class UserController {
         return ApiResponse.success(userService.getSystemUsersById(userId));
     }
 
-//    @PutMapping(value = "/{userId}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-//    @PreAuthorize("hasRole('ADMIN')")
-//    public ApiResponse<UserResponse> updateUserProfileById(
-//            @PathVariable Long userId,
-//            @RequestPart("user") String userStr,
-//            @RequestPart(value = "file", required = false) MultipartFile file) throws IOException {
-//
-//        UpdateUserRequest updateUserRequest = ValidationUtil.validateUserStr(userStr);
-//        return ApiResponse.success(userService.updateUserProfileById(updateUserRequest, file, userId));
-//    }
+    //    @PutMapping(value = "/{userId}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    //    @PreAuthorize("hasRole('ADMIN')")
+    //    public ApiResponse<UserResponse> updateUserProfileById(
+    //            @PathVariable Long userId,
+    //            @RequestPart("user") String userStr,
+    //            @RequestPart(value = "file", required = false) MultipartFile file) throws IOException {
+    //
+    //        UpdateUserRequest updateUserRequest = ValidationUtil.validateUserStr(userStr);
+    //        return ApiResponse.success(userService.updateUserProfileById(updateUserRequest, file, userId));
+    //    }
 
-//    @PostMapping(value = "/admins", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-//    @PreAuthorize("hasRole('ADMIN')")
-//    public ApiResponse<UserResponse> createSystemUserProfile(
-//            @RequestPart("user") String userStr,
-//            @RequestPart(value = "file", required = false) MultipartFile file) throws IOException {
-//
-//        CreateSystemUserRequest request = ValidationUtil.validateSystemUserStr(userStr);
-//        return ApiResponse.success(userService.createSystemUser(request, file));
-//    }
+    //    @PostMapping(value = "/admins", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    //    @PreAuthorize("hasRole('ADMIN')")
+    //    public ApiResponse<UserResponse> createSystemUserProfile(
+    //            @RequestPart("user") String userStr,
+    //            @RequestPart(value = "file", required = false) MultipartFile file) throws IOException {
+    //
+    //        CreateSystemUserRequest request = ValidationUtil.validateSystemUserStr(userStr);
+    //        return ApiResponse.success(userService.createSystemUser(request, file));
+    //    }
 
-//    @PutMapping(value = "/admins/{userId}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-//    @PreAuthorize("hasRole('ADMIN')")
-//    public ApiResponse<UserResponse> updateSystemUserProfile(
-//            @PathVariable Long userId,
-//            @RequestPart("user") String userStr,
-//            @RequestPart(value = "file", required = false) MultipartFile file) throws IOException {
-//
-//        UpdateSystemUserRequest request = ValidationUtil.validateUpdateSystemUserStr(userStr);
-//        return ApiResponse.success(userService.updateSystemUser(request, file, userId));
-//    }
+    //    @PutMapping(value = "/admins/{userId}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    //    @PreAuthorize("hasRole('ADMIN')")
+    //    public ApiResponse<UserResponse> updateSystemUserProfile(
+    //            @PathVariable Long userId,
+    //            @RequestPart("user") String userStr,
+    //            @RequestPart(value = "file", required = false) MultipartFile file) throws IOException {
+    //
+    //        UpdateSystemUserRequest request = ValidationUtil.validateUpdateSystemUserStr(userStr);
+    //        return ApiResponse.success(userService.updateSystemUser(request, file, userId));
+    //    }
 
     @DeleteMapping("/customers")
     @PreAuthorize("hasRole('ADMIN')||hasRole('STAFF')")
@@ -128,51 +128,51 @@ public class UserController {
         return ApiResponse.success(null);
     }
 
-
-//    @GetMapping
-//    ApiResponse<List<UserResponse>> getUsers() {
-//        var authentication = SecurityContextHolder.getContext().getAuthentication();
-//
-//        log.info("Username {}", authentication.getName());
-//        authentication.getAuthorities().forEach(grantedAuthority -> log.info("GrantedAuthority {}", grantedAuthority.getAuthority()));
-//
-//        return (ApiResponse.<List<UserResponse>>builder()
-//                .result(userService.getUsers())
-//                .build());
-//
-//    }
-//
-//    @GetMapping("/page")
-//    public PaginatedApiResponse<UserResponse> getUsers(
-//            @RequestParam(value = "page", defaultValue = "0") int page,
-//            @RequestParam(value = "size", defaultValue = "10") int size) {
-//        Pageable pageable = PageRequest.of(page, size);
-//        PaginatedApiResponse<UserResponse> response = userService.getUsers(pageable);
-//
-//
-//        return response;
-//    }
-//
-//    @GetMapping("/{userId}")
-//    ApiResponse<UserResponse> getUser(@PathVariable("userId") String userId) {
-//        ApiResponse<UserResponse> apiResponse = new ApiResponse<>();
-//        apiResponse.setCode(1000);
-//        apiResponse.setResult(userService.getUserRespone(userId));
-//        return apiResponse;
-//    }
-//
-//    @PutMapping("/{userId}")
-//    ApiResponse<UserResponse> updateUser(@PathVariable String userId, @RequestBody UserUpdateRequest request) {
-//        ApiResponse<UserResponse> apiResponse = new ApiResponse<>();
-//        apiResponse.setCode(1000);
-//        apiResponse.setResult(userService.updateUser(userId, request));
-//        return apiResponse;
-//    }
-//
-//    @DeleteMapping("/{userId}")
-//    String deleteUser(@PathVariable String userId) {
-//        userService.deleteUser(userId);
-//        return "User has been deleted";
-//    }
+    //    @GetMapping
+    //    ApiResponse<List<UserResponse>> getUsers() {
+    //        var authentication = SecurityContextHolder.getContext().getAuthentication();
+    //
+    //        log.info("Username {}", authentication.getName());
+    //        authentication.getAuthorities().forEach(grantedAuthority -> log.info("GrantedAuthority {}",
+    // grantedAuthority.getAuthority()));
+    //
+    //        return (ApiResponse.<List<UserResponse>>builder()
+    //                .result(userService.getUsers())
+    //                .build());
+    //
+    //    }
+    //
+    //    @GetMapping("/page")
+    //    public PaginatedApiResponse<UserResponse> getUsers(
+    //            @RequestParam(value = "page", defaultValue = "0") int page,
+    //            @RequestParam(value = "size", defaultValue = "10") int size) {
+    //        Pageable pageable = PageRequest.of(page, size);
+    //        PaginatedApiResponse<UserResponse> response = userService.getUsers(pageable);
+    //
+    //
+    //        return response;
+    //    }
+    //
+    //    @GetMapping("/{userId}")
+    //    ApiResponse<UserResponse> getUser(@PathVariable("userId") String userId) {
+    //        ApiResponse<UserResponse> apiResponse = new ApiResponse<>();
+    //        apiResponse.setCode(1000);
+    //        apiResponse.setResult(userService.getUserRespone(userId));
+    //        return apiResponse;
+    //    }
+    //
+    //    @PutMapping("/{userId}")
+    //    ApiResponse<UserResponse> updateUser(@PathVariable String userId, @RequestBody UserUpdateRequest request) {
+    //        ApiResponse<UserResponse> apiResponse = new ApiResponse<>();
+    //        apiResponse.setCode(1000);
+    //        apiResponse.setResult(userService.updateUser(userId, request));
+    //        return apiResponse;
+    //    }
+    //
+    //    @DeleteMapping("/{userId}")
+    //    String deleteUser(@PathVariable String userId) {
+    //        userService.deleteUser(userId);
+    //        return "User has been deleted";
+    //    }
 
 }
