c84708807aaf2d48fcc199e354cc63f0c46545e8
towh2301 <huy.buihoang2301@gmail.com>
2024-08-24 20:48:47 +0700

    add create "admin" role

--- src/main/java/com/thanhtan/identity/config/ApplicationInitConfig.java
+++ src/main/java/com/thanhtan/identity/config/ApplicationInitConfig.java
@@ -2,6 +2,7 @@ package com.thanhtan.identity.config;
 
 import java.util.HashSet;
 
+import com.thanhtan.identity.repository.RoleRepository;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,6 +17,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
 
+import static java.rmi.server.LogStream.log;
+
 @Configuration
 @RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
@@ -23,22 +26,32 @@ import lombok.extern.slf4j.Slf4j;
 public class ApplicationInitConfig {
 
     PasswordEncoder passwordEncoder;
+    RoleRepository roleRepository;
 
     @Bean
     ApplicationRunner applicationRunner(UserRepository userRepository) {
         return args -> {
             if (userRepository.findByUsername("admin").isEmpty()) {
-                var roles = new HashSet<String>();
-                roles.add(Role.ADMIN.name());
+                // Set default role for admin user
+                // If the role is not existed, create it
 
+                HashSet<com.thanhtan.identity.entity.Role> roles = new HashSet<>();
+                roles.add(roleRepository.findByName("ADMIN").orElseGet(() -> {
+                    log("ADMIN role not found. Creating it.");
+                    com.thanhtan.identity.entity.Role role = new com.thanhtan.identity.entity.Role();
+                    role.setName("ADMIN");
+                    return roleRepository.save(role);
+                }));
+
+                //orElseThrow(()->new AppException(ErrorCode.ROLE_NOT_EXISTED))
                 User user = User.builder()
                         .username("admin")
                         .password(passwordEncoder.encode("admin"))
-                        //                        .roles(roles)
+                        .roles(roles)
                         .build();
 
                 userRepository.save(user);
-                log.warn("admin user has been created with default password: admin, please change it");
+                ApplicationInitConfig.log.warn("admin user has been created with default password: admin, please change it");
             }
             //            emailSender.sendTestEmail();
         };
