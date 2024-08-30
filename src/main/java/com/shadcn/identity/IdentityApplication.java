4b294e95a2bcbf1865177e8c697fd3fa0eea897c
towh2301 <huy.buihoang2301@gmail.com>
2024-08-30 23:35:39 +0700

    commit

--- src/main/java/com/shadcn/identity/IdentityApplication.java
+++ src/main/java/com/shadcn/identity/IdentityApplication.java
@@ -7,7 +7,6 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 @EnableFeignClients
 public class IdentityApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(IdentityApplication.class, args);
     }
