3c87f4d8c36d7b4c83a16000a0638ac1c7170060
TanNgo11 <131520745+TanNgo11@users.noreply.github.com>
2024-09-01 10:48:12 +0700

    Revert "commit"
    This reverts commit c35c4c3ba41be8c7d96a9d626606a3d29e78b5fd.

--- src/main/java/com/shadcn/identity/IdentityApplication.java
+++ src/main/java/com/shadcn/identity/IdentityApplication.java
@@ -7,6 +7,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 @EnableFeignClients
 public class IdentityApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(IdentityApplication.class, args);
     }
