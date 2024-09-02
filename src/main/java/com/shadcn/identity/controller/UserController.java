40ee66ad55f25f45cf8c21ca5e2fb244757d850e
TanNgo11 <tan.ngo.cit20@eiu.edu.vn>
2024-09-02 14:26:31 +0700

    feat/cleaner-identity-service

--- src/main/java/com/shadcn/identity/controller/UserController.java
+++ src/main/java/com/shadcn/identity/controller/UserController.java
@@ -1,28 +1,20 @@
 package com.shadcn.identity.controller;
 
-import static com.shadcn.identity.constant.PathConstant.API_V1_USERS;
-
-import java.util.List;
-
-import com.cloudinary.Api;
-import com.shadcn.identity.entity.User;
-import com.shadcn.identity.service.IAuthenticationService;
-import jakarta.validation.Valid;
-
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
-
 import com.shadcn.identity.dto.request.UserCreationRequest;
 import com.shadcn.identity.dto.response.ApiResponse;
 import com.shadcn.identity.dto.response.UserResponse;
 import com.shadcn.identity.service.IUserService;
-
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import jakarta.validation.Valid;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import static com.shadcn.identity.constant.PathConstant.API_V1_USERS;
 
 @RestController
 @RequestMapping(API_V1_USERS)
@@ -45,143 +37,11 @@ public class UserController {
         return ApiResponse.success(userService.getMyInfo());
     }
 
-    //    @PutMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    //    @PreAuthorize("hasRole('USER')||hasRole('ADMIN')")
-    //    public ApiResponse<UserResponse> updateUserProfile(
-    //            @RequestPart("user") String userStr,
-    //            @RequestPart(value = "file", required = false) MultipartFile file) throws IOException {
-    //        UpdateUserRequest updateUserRequest = ValidationUtil.validateUserStr(userStr);
-    //
-    //        return ApiResponse.success(userService.updateUserProfile(updateUserRequest, file));
-    //    }
-
-    @GetMapping
-    @PreAuthorize("hasRole('ADMIN')||hasRole('STAFF')")
-    ApiResponse<List<UserResponse>> getAllUsers() {
-        return ApiResponse.success(userService.getUsers());
-    }
-
-    @GetMapping("/customers")
-    @PreAuthorize("hasRole('ADMIN')||hasRole('STAFF')")
-    ApiResponse<List<UserResponse>> getAllCustomer() {
-        return ApiResponse.success(userService.getAllCustomer());
-    }
-
-    @GetMapping("/admin")
-    @PreAuthorize("hasRole('ADMIN')")
-    ApiResponse<List<UserResponse>> getAllAdmin() {
-        return ApiResponse.success(userService.getAllAdmin());
-    }
-
-    @GetMapping("/{userId}")
-    @PreAuthorize("hasRole('ADMIN')||hasRole('STAFF')")
-    ApiResponse<UserResponse> getCustomerById(@PathVariable Long userId) {
-        return ApiResponse.success(userService.getCustomerById(userId));
-    }
-
-    @GetMapping("/admins/{userId}")
-    @PreAuthorize("hasRole('ADMIN')")
-    ApiResponse<UserResponse> getSystemUsersById(@PathVariable Long userId) {
-        return ApiResponse.success(userService.getSystemUsersById(userId));
-    }
 
     @PostMapping("/verify-email")
-    @PreAuthorize("hasRole('ADMIN')||hasRole('STAFF')||hasRole('USER')")
     public ApiResponse<UserResponse> verifyEmail(@RequestParam("email") String email) {
         return ApiResponse.success(userService.verifyEmail(email));
     }
 
-    //    @PutMapping(value = "/{userId}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    //    @PreAuthorize("hasRole('ADMIN')")
-    //    public ApiResponse<UserResponse> updateUserProfileById(
-    //            @PathVariable Long userId,
-    //            @RequestPart("user") String userStr,
-    //            @RequestPart(value = "file", required = false) MultipartFile file) throws IOException {
-    //
-    //        UpdateUserRequest updateUserRequest = ValidationUtil.validateUserStr(userStr);
-    //        return ApiResponse.success(userService.updateUserProfileById(updateUserRequest, file, userId));
-    //    }
-
-    //    @PostMapping(value = "/admins", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    //    @PreAuthorize("hasRole('ADMIN')")
-    //    public ApiResponse<UserResponse> createSystemUserProfile(
-    //            @RequestPart("user") String userStr,
-    //            @RequestPart(value = "file", required = false) MultipartFile file) throws IOException {
-    //
-    //        CreateSystemUserRequest request = ValidationUtil.validateSystemUserStr(userStr);
-    //        return ApiResponse.success(userService.createSystemUser(request, file));
-    //    }
-
-    //    @PutMapping(value = "/admins/{userId}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    //    @PreAuthorize("hasRole('ADMIN')")
-    //    public ApiResponse<UserResponse> updateSystemUserProfile(
-    //            @PathVariable Long userId,
-    //            @RequestPart("user") String userStr,
-    //            @RequestPart(value = "file", required = false) MultipartFile file) throws IOException {
-    //
-    //        UpdateSystemUserRequest request = ValidationUtil.validateUpdateSystemUserStr(userStr);
-    //        return ApiResponse.success(userService.updateSystemUser(request, file, userId));
-    //    }
-
-    @DeleteMapping("/customers")
-    @PreAuthorize("hasRole('ADMIN')||hasRole('STAFF')")
-    public ApiResponse<String> deleteCustomers(@RequestParam Long[] ids) {
-        userService.deleteUserssByIds(ids);
-        return ApiResponse.success(null);
-    }
-
-    @DeleteMapping("/admins")
-    @PreAuthorize("hasRole('ADMIN')")
-    public ApiResponse<String> deleteAdmins(@RequestParam Long[] ids) {
-        userService.deleteUserssByIds(ids);
-        return ApiResponse.success(null);
-    }
-
-    //    @GetMapping
-    //    ApiResponse<List<UserResponse>> getUsers() {
-    //        var authentication = SecurityContextHolder.getContext().getAuthentication();
-    //
-    //        log.info("Username {}", authentication.getName());
-    //        authentication.getAuthorities().forEach(grantedAuthority -> log.info("GrantedAuthority {}",
-    // grantedAuthority.getAuthority()));
-    //
-    //        return (ApiResponse.<List<UserResponse>>builder()
-    //                .result(userService.getUsers())
-    //                .build());
-    //
-    //    }
-    //
-    //    @GetMapping("/page")
-    //    public PaginatedApiResponse<UserResponse> getUsers(
-    //            @RequestParam(value = "page", defaultValue = "0") int page,
-    //            @RequestParam(value = "size", defaultValue = "10") int size) {
-    //        Pageable pageable = PageRequest.of(page, size);
-    //        PaginatedApiResponse<UserResponse> response = userService.getUsers(pageable);
-    //
-    //
-    //        return response;
-    //    }
-    //
-    //    @GetMapping("/{userId}")
-    //    ApiResponse<UserResponse> getUser(@PathVariable("userId") String userId) {
-    //        ApiResponse<UserResponse> apiResponse = new ApiResponse<>();
-    //        apiResponse.setCode(1000);
-    //        apiResponse.setResult(userService.getUserRespone(userId));
-    //        return apiResponse;
-    //    }
-    //
-    //    @PutMapping("/{userId}")
-    //    ApiResponse<UserResponse> updateUser(@PathVariable String userId, @RequestBody UserUpdateRequest request) {
-    //        ApiResponse<UserResponse> apiResponse = new ApiResponse<>();
-    //        apiResponse.setCode(1000);
-    //        apiResponse.setResult(userService.updateUser(userId, request));
-    //        return apiResponse;
-    //    }
-    //
-    //    @DeleteMapping("/{userId}")
-    //    String deleteUser(@PathVariable String userId) {
-    //        userService.deleteUser(userId);
-    //        return "User has been deleted";
-    //    }
 
 }
--- src/main/java/com/shadcn/identity/service/IUserService.java
+++ src/main/java/com/shadcn/identity/service/IUserService.java
@@ -1,9 +1,5 @@
 package com.shadcn.identity.service;
 
-import java.text.ParseException;
-import java.util.List;
-
-import com.nimbusds.jose.JOSEException;
 import com.shadcn.identity.dto.request.UserCreationRequest;
 import com.shadcn.identity.dto.response.UserResponse;
 
@@ -12,26 +8,5 @@ public interface IUserService {
 
     UserResponse getMyInfo();
 
-    //    UserResponse updateUserProfile(UpdateUserRequest request, MultipartFile file);
-    //
-    //    UserResponse createSystemUser(CreateSystemUserRequest request, MultipartFile file);
-    //
-    //    UserResponse updateSystemUser(UpdateSystemUserRequest request, MultipartFile file, Long userId);
-    //
-    //    UserResponse updateUserProfileById(UpdateUserRequest request, MultipartFile file, Long userId);
-
-    List<UserResponse> getUsers();
-
-    List<UserResponse> getAllCustomer();
-
-    UserResponse getCustomerById(Long userId);
-
-    UserResponse getSystemUsersById(Long userId);
-
-    List<UserResponse> getAllAdmin();
-
-    void deleteUserssByIds(Long[] ids);
-
-    // Verify Email using Link
     UserResponse verifyEmail(String email);
 }
