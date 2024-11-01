74ec179041785f4e34fc25a4a518f38b2ca71ee6
TanNgo11 <tan_ngo@datahouse.com>
2024-11-01 21:17:00 +0700

    fix/change-url-web-FE

--- src/main/java/com/shadcn/identity/config/WebConfig.java
+++ src/main/java/com/shadcn/identity/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         log.info("Configuring CORS");
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://localhost:5173")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
